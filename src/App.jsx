9c3c50245370570b513c2d642de829b6250e3870
Roman <roman.markanych@gmail.com>
2022-09-10 22:58:05 +0300

    add 1 task

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Profile from './components/Profile';
+import user from './user.json';
+
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 24,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};

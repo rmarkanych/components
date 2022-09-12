8de1c0037da23e5cf4e96df26af7584bd1a31b0a
Roman <roman.markanych@gmail.com>
2022-09-12 20:16:51 +0300

    add styles for table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Profile from './components/profile/Profile';
 import Statistics from './components/statistics/Statistics';
-import FriendList from './components/friends/FriendList'
+import FriendList from './components/friends/FriendList';
 import TransactionHistory from 'components/transactions/Transactions';
-import user from './user.json';
-import data from './data.json';
-import friends from './friends.json'
-import transactions from './transactions.json'
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
- const App = () => 
-   <div style={{ padding: "15px",display:"flex",gap:"20px"}}>
+const App = () => {
+  return (
+    <>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -16,9 +17,11 @@ import transactions from './transactions.json'
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data}/>
-     <FriendList friends={friends} />
-     <TransactionHistory items={transactions} />
-    </div>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,29 +1,36 @@
-import PropTypes from "prop-types";
-import css from './Statistics.module.css'
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
 const Statistics = ({ stats }) => {
   return (
-<section className={css.statistics}>
-  <h2 className={css.title}>Upload stats</h2>
-    <ul className={css.list}>
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
+      <ul className={css.list}>
         {stats.map(({ id, label, percentage }) => {
           return (
-          <li className={css.item} key={id}>
-            <span className={css.label}>{label}</span>
-            <span className={css.percentage}>{percentage}%</span>
-          </li>
-        )
-      }   
-      )}
-    </ul>
+            <li
+              className={css.item}
+              key={id}
+              style={{
+                backgroundColor: `#${Math.floor(
+                  Math.random() * 16777215
+                ).toString(16)}`,
+              }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
     </section>
-  )
-}
+  );
+};
 
 Statistics.propTypes = {
   id: PropTypes.string,
-  label:PropTypes.string,
-  percentage:PropTypes.number,
-}
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
 
-export default Statistics
+export default Statistics;
